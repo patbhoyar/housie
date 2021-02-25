be052043120cb95249e2118757e625a957cf2dbd
Parthiva <patbhoyar18@gmail.com>
2021-02-26 01:47:09 +0530

    some cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@material-ui/core';
 import React, { FunctionComponent, useState } from 'react';
 import './App.css';
 import { NumberTile } from './number-tile';
@@ -6,18 +7,20 @@ const App: FunctionComponent<{}> = (props) => {
 
   const TOTAL_NUMBERS = 90;
   const rules = ['First Line', 'Second Line', 'Third Line', 'BP', 'Full House'];
-  const allNumbers = Array.from(Array(TOTAL_NUMBERS).keys()).map((i: number) => i+1);
+  const allNumbers = Array.from(Array(TOTAL_NUMBERS).keys()).map((i: number) => i + 1);
   const [doneNumbers, setDoneNumbers] = useState<number[]>([]);
   const [remainingNumbers, setRemainingNumbers] = useState<number[]>(allNumbers);
   const [generatedNumber, setGeneratedNumber] = useState<number>(0);
   const [lastNumber, setLastNumber] = useState<number>(0);
-  let availableNumbers = Array.from(Array(TOTAL_NUMBERS).keys()).map((i: number) => i+1);
+  const [selectedRules, setList] = useState<Array<boolean>>(Array(rules.length).fill(true));
+  const [appliedRules, setRules] = useState<Array<string>>(rules);
+  let availableNumbers = Array.from(Array(TOTAL_NUMBERS).keys()).map((i: number) => i + 1);
 
   const displayTiles = allNumbers.map((i: number) => {
     return (
       <>
-        <NumberTile displayNumber={i} isDone={doneNumbers.includes(i)}></NumberTile>
-        { i%10 === 0 ? <br /> : null }
+        <NumberTile displayNumber={i} isDone={doneNumbers.includes(i)} isCurrentNumber={i === generatedNumber}></NumberTile>
+        { i % 10 === 0 ? <br /> : null}
       </>
     )
   });
@@ -30,26 +33,44 @@ const App: FunctionComponent<{}> = (props) => {
       setGeneratedNumber(newNumber);
       availableNumbers = [...remainingNumbers];
       availableNumbers.splice(index, 1);
-      setRemainingNumbers(availableNumbers)
+      setRemainingNumbers(availableNumbers);
       setDoneNumbers([...doneNumbers, newNumber]);
     }
   }
 
-  const displayRules = rules.map((rule: string) => {
+  const onCheck = (e: any, index: number) => {
+    const x = [...selectedRules];
+    x[index] = e.target.checked;
+    setList(x);
+    const a: string[] = [];
+    x.forEach((isSelected: boolean, i: number) => {
+      if (isSelected) {
+        a.push(rules[i]);
+      }
+    });
+    setRules(a)
+  }
+
+  const displayRules = rules.map((rule: string, index: number) => {
+    const forCheck = rule.split(" ").join("_");
     return (
       <span>
-        <input type="checkbox" id={rule} name={rule}/>
-        <label for={rule}>{rule}</label>
-        <br/>
+        <input type="checkbox" id={forCheck} name={forCheck} onChange={(e) => onCheck(e, index)} checked={selectedRules[index]} />
+        <label htmlFor={forCheck}>{rule}</label>
+        <br />
       </span>
     );
+  });
+
+  const renderRules = () => appliedRules.map((rule: string) => {
+    return (<li>{rule}</li>)
   });
 
   return (
     <div className="App">
       <div id="header">
         <div className='generatedNumberContainer'>
-          <button onClick={generateNumber} disabled={remainingNumbers.length === 0} id="generateButton">Generate</button>
+          <Button variant="contained" color="primary" onClick={generateNumber} disabled={remainingNumbers.length === 0} >Generate</Button>
           <div className='generatedNumber'>{generatedNumber}</div>
         </div>
         <div className='lastNumberContainer'>
@@ -60,11 +81,15 @@ const App: FunctionComponent<{}> = (props) => {
           {displayRules}
         </div>
         <div id="notes">
-          <textarea rows="9" cols="50"></textarea>
+          <textarea rows={9} cols={50}></textarea>
         </div>
       </div>
       <div id="tileContainer">
         {displayTiles}
+      </div>
+      <div id="ruleContainer">
+        <div id="remainingTitle"><u>Remaining Plays</u></div>
+        {renderRules()}
       </div>
     </div>
   );
--- src/number-tile/index.tsx
+++ src/number-tile/index.tsx
@@ -4,10 +4,11 @@ import './index.css';
 interface Props {
   displayNumber: number;
   isDone: boolean;
+  isCurrentNumber: boolean;
 }
 
 export const NumberTile: FunctionComponent<Props> = (props) => {
   return (
-    <span className={`tile${props.isDone ? " done" : ""}`}>{props.displayNumber}</span>
+    <span className={`tile${props.isDone ? " done" : ""}${props.isCurrentNumber ? " current" : ""}`}>{props.displayNumber}</span>
   );
 }
